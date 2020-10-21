b5a641148d8b182a5e3c689b206f36349ea90265
tana <charleatanasi@gmail.com>
2020-10-21 12:32:59 +0300

    NavigationView click listener

--- app/src/main/java/com/tana/navdrawer/MainActivity.java
+++ app/src/main/java/com/tana/navdrawer/MainActivity.java
@@ -1,14 +1,111 @@
 package com.tana.navdrawer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar mToolbar;
+    DrawerLayout mDrawer;
+    NavigationView mNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawer = findViewById(R.id.drawer_layout);
+        mNavView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        mNavView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_message:
+                handleMessage();
+                break;
+            case R.id.nav_chat:
+                handleChat();
+                break;
+            case R.id.nav_profile:
+                try {
+                    handleProfile();
+                } catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.nav_share:
+                handleShare();
+                break;
+            case R.id.nav_send:
+                handleSend();
+        }
+        return true;
+    }
+
+    private void handleSend() {
+        Toast.makeText(this, "Data Sent", Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleShare() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+
+    private void handleProfile() throws PackageManager.NameNotFoundException {
+        this.getPackageManager().getPackageInfo("com.twitter.android", 0);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?MagimbiJr=USERID"));
+        intent.putExtra("user_id", 634163427L);
+        startActivity(intent);
+    }
+
+    private void handleChat() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setType("vnd.android-dir/mms-sms");
+        startActivity(intent);
+    }
+
+    private void handleMessage() {
+        String subject = "Great the World";
+        String text = "Hello World!.. Please be safe out there";
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc2822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(intent);
+
     }
 }
